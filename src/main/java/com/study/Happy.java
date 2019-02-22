3b682e200c409c806260c796ad04f8bdd7ac9146
yankun.w <yankun.w@pcitc.com>
2019-02-22 17:20:04 +0800

    sorry commit again

--- src/main/java/com/study/Happy.java
+++ src/main/java/com/study/Happy.java
@@ -3,6 +3,6 @@ package com.study;
 public class Happy {
 
 		public static void main(String[] args) {
-			System.out.println("happy lisi aaaa");
+			System.out.println("happy lisi zhangsan");
 		}
 }
