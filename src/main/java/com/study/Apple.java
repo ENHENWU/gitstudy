161bc3f983549f40f13c0387fa3926dc131f0c38
yankun.w <yankun.w@pcitc.com>
2019-02-22 16:37:09 +0800

    first commit eclipse

---
+++ src/main/java/com/study/Apple.java
@@ -0,0 +1,13 @@
+package com.study;
+
+public class Apple {
+private String name;
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+}
---
+++ src/test/java/gitstudy/TestApple.java
@@ -0,0 +1,10 @@
+package gitstudy;
+
+public class TestApple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
